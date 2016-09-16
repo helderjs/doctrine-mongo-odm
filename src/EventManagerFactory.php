7e548eea330e039a4b3eb5f6c91075edd1a93171
Helder Santana <contato@heldersantana.net>
2016-09-16 14:28:02 +0200

    Add the EventManager factory

---
+++ src/EventManagerFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Helderjs\Component\DoctrineMongoODM;
+
+use Doctrine\Common\EventManager;
+use Doctrine\Common\EventSubscriber;
+use Helderjs\Component\DoctrineMongoODM\Exception\InvalidConfigException;
+use Interop\Container\ContainerInterface;
+
+class EventManagerFactory extends AbstractFactory
+{
+    /**
+     * @param ContainerInterface $container
+     * @return EventManager
+     * @throws InvalidConfigException for invalid config service values.
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $options = $this->getDoctrineConfiguration($container, 'eventmanager');
+        $eventManager = new EventManager();
+
+        if (empty($options)) {
+            return $eventManager;
+        }
+
+        try {
+            foreach ($options['subscribers'] as $subscriberName) {
+                $subscriber = $subscriberName;
+                if (is_string($subscriber)) {
+                    if ($container->has($subscriber)) {
+                        $subscriber = $container->get($subscriber);
+                    } elseif (class_exists($subscriber)) {
+                        $subscriber = new $subscriber();
+                    }
+                }
+                if ($subscriber instanceof EventSubscriber) {
+                    $eventManager->addEventSubscriber($subscriber);
+                    continue;
+                }
+                $subscriberType = is_object($subscriberName) ? get_class($subscriberName) : $subscriberName;
+                throw new InvalidConfigException(
+                    sprintf(
+                        'Invalid event subscriber "%s" given, must be a service name, '
+                        . 'class name or an instance implementing Doctrine\Common\EventSubscriber',
+                        is_string($subscriberType) ? $subscriberType : gettype($subscriberType)
+                    )
+                );
+            }
+        } catch (\Exception $e) {
+            throw new InvalidConfigException($e->getMessage());
+        }
+
+        return $eventManager;
+    }
+}
---
+++ test/EventManagerFactoryTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Helderjs\Test\Component\DoctrineMongoODM;
+
+use Doctrine\Common\EventManager;
+use Helderjs\Component\DoctrineMongoODM\EventManagerFactory;
+use Helderjs\Component\DoctrineMongoODM\Exception\InvalidConfigException;
+use Helderjs\Test\Component\DoctrineMongoODM\Asset\TestEventSubscriber;
+use Helderjs\Test\Component\DoctrineMongoODM\Asset\TestEventSubscriber2;
+use Helderjs\Test\Component\DoctrineMongoODM\Asset\TestEventSubscriber3;
+use Interop\Container\ContainerInterface;
+
+class EventManagerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+    }
+
+    public function testCallingFactoryWithNoConfigReturns()
+    {
+        $factory = new EventManagerFactory();
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(false);
+        $eventManager = $factory($this->container->reveal());
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn([]);
+        $eventManager = $factory($this->container->reveal());
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->get('doctrine')->willReturn([]);
+        $this->container->has('config')->willReturn(true);
+        $eventManager = $factory($this->container->reveal());
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+    }
+
+    public function testCallingFactoryWithEmptyDoctrineConfig()
+    {
+        $factory = new EventManagerFactory();
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn(['doctrine' => []]);
+        $eventManager = $factory($this->container->reveal());
+
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+    }
+
+    public function testCallingFactoryWithWrongDoctrineConfig()
+    {
+        $factory = new EventManagerFactory();
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn(['doctrine' => ['eventmanager' => []]]);
+        $eventManager = $factory($this->container->reveal());
+
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+    }
+
+    public function testCallingFactoryWithDoctrineConfigWrongSubscriber()
+    {
+        $options = $options = [
+            'doctrine' => [
+                'default' => 'odm_default',
+                'eventmanager' => [
+                    'odm_default' => [
+                        'subscribers' => [
+                            true,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $factory = new EventManagerFactory();
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($options);
+        $this->expectException(InvalidConfigException::class);
+        $factory($this->container->reveal());
+    }
+
+    public function testCallingFactoryWithDoctrineConfig()
+    {
+        $subscriber = $this->prophesize(TestEventSubscriber::class);
+
+        $options = $options = [
+            'doctrine' => [
+                'default' => 'odm_default',
+                'eventmanager' => [
+                    'odm_default' => [
+                        'subscribers' => [
+                            TestEventSubscriber::class,
+                            TestEventSubscriber2::class,
+                            new TestEventSubscriber3(),
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $factory = new EventManagerFactory();
+
+        $this->container->has('doctrine')->willReturn(false);
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($options);
+        $this->container->has(TestEventSubscriber::class)->willReturn(true);
+        $this->container->get(TestEventSubscriber::class)->willReturn($subscriber->reveal());
+        $this->container->has(TestEventSubscriber2::class)->willReturn(false);
+        $this->container->has(TestEventSubscriber3::class)->willReturn(false);
+        $eventManager = $factory($this->container->reveal());
+
+        $this->assertInstanceOf(EventManager::class, $eventManager);
+    }
+}
